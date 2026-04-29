6ac372c357230111788bd30e6f7944530c70fd19
Hannah Cushman Garland <hannah.cushman@datamade.us>
2026-04-29 10:14:26 -0500

    Update spider for 2026

--- data/is_the_pool_open/settings.py
+++ data/is_the_pool_open/settings.py
@@ -13,8 +13,7 @@ SPIDER_MODULES = ["is_the_pool_open.spiders"]
 NEWSPIDER_MODULE = "is_the_pool_open.spiders"
 
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-# USER_AGENT = "is_the_pool_open (+http://www.yourdomain.com)"
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -25,7 +24,9 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-# DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 5
+CONCURRENT_REQUESTS = 1
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
@@ -81,13 +82,16 @@ ROBOTSTXT_OBEY = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-# HTTPCACHE_ENABLED = True
-# HTTPCACHE_EXPIRATION_SECS = 0
-# HTTPCACHE_DIR = "httpcache"
-# HTTPCACHE_IGNORE_HTTP_CODES = []
-# HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 60 * 60 * 24 * 7
+HTTPCACHE_DIR = "httpcache"
+HTTPCACHE_IGNORE_HTTP_CODES = [202]
+HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
 
 # Set settings whose default value is deprecated to a future-proof value
+RETRY_TIMES = 10
+RETRY_HTTP_CODES = [202]
+
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
--- data/is_the_pool_open/spiders/chi_parks_pool_scraper.py
+++ data/is_the_pool_open/spiders/chi_parks_pool_scraper.py
@@ -1,83 +1,85 @@
-import unicodedata
-from urllib.parse import urlencode
-
 from dateutil.parser import parse as parse_date
+
 import scrapy
 
-
-BASE_URL = "https://www.chicagoparkdistrict.com"
-FACILITIES_URL = f"{BASE_URL}/parks-facilities"
+BASE_URL = "www.chicagoparkdistrict.com"
 
 
 class ChiParksPoolSpider(scrapy.Spider):
     name = "chi_parks_pool_spider"
     allowed_domains = [BASE_URL]
-    start_urls = [
-        f"{FACILITIES_URL}/?{urlencode({'field_location_type_target_id[]': 148})}"
-    ]
-
+    start_urls = [f"https://{BASE_URL}/facilities/swimming-pools"]
     def get_address(self, response):
-        address_lines = response.xpath("//p[@class='address']/text()").getall()[:2]
+        address_lines = response.xpath("//p[@class='address']/span/text()").getall()[:2]
         return ", ".join(line.strip() for line in address_lines)
 
     def get_phone_number(self, response):
         phone_number = response.xpath(
-            "//div[contains(@class, 'phone')]/span[@class='location-header-value']/text()"
+            "//div[contains(@class, 'paragraph--type--phone-number')]/text()"
         ).get()
 
         if phone_number:
-            return phone_number.replace("\n", "").replace("Office", "").strip()
-
-    def get_alert(self, response):
-        alert_elements = response.xpath(
-            "//div[@role='alert']/descendant::*/text()"
-        ).getall()
-
-        if alert_elements:
-            return ", ".join(
-                unicodedata.normalize("NFKD", piece) for piece in alert_elements[2:]
+            return (
+                phone_number.replace("\n", "")
+                .replace("Tel: ", "")
+                .replace("|", "")
+                .strip()
             )
 
+    def get_alert(self, response):
+        return
+
     def parse_pool(self, response):
-        schedule = response.xpath(
-            "//section[@id='block-views-block-loc-sect-seg-location-park-hours']//tr[contains(@class, 'office-hours__item')]/td"
-        )
+
+        schedule_pdf_urls = response.xpath(
+            "//a[contains(translate(@href, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), 'schedule') and contains(@href, '.pdf')]/@href"
+        ).getall()
 
         schedule_obj = {}
 
-        for idx, cell in enumerate(schedule):
-            if cell.attrib["class"] == "office-hours__item-label":
-                day = cell.xpath("text()").get().rstrip(": ")
-                hours = schedule[idx + 1].xpath("text()").get()
+        if schedule_pdf_urls:
+            schedule = response.xpath(
+                "//div[contains(@class, 'office-hours')]//div[contains(@class, 'office-hours__item')]/span"
+            )
 
-                try:
-                    open, close = hours.split("-")
-                except (ValueError, AttributeError):
-                    schedule_obj[day] = {}
-                else:
-                    schedule_obj[day] = {
-                        "open": parse_date(open).time(),
-                        "close": parse_date(close).time(),
-                    }
+            for idx, cell in enumerate(schedule):
+                if cell.attrib.get("class") == "office-hours__item-label":
+                    day = cell.xpath("text()").get().rstrip(": ")
+                    
+                    # Skip general hours
+                    if day == " |":
+                        continue
 
-        pool_location_xpath = "//span[@class='location-header-value' and contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '{pool_location}')]"
+                    hours = schedule[idx + 1].xpath("text()").get()
+
+                    try:
+                        open, close = hours.split("-")
+                    except (ValueError, AttributeError):
+                        schedule_obj[day] = {}
+                    else:
+                        schedule_obj[day] = {
+                            "open": parse_date(open).time(),
+                            "close": parse_date(close).time(),
+                        }
+                        
+        additional_details = "".join(
+            response.xpath(
+                "//div[@class='facility-extra-details']/div[contains(@class, 'field--label-above')]/text()"
+            ).getall()
+        ).lower()
 
         yield {
-            "name": response.xpath("//h1/text()").get(),
+            "name": response.xpath(
+                "//span[contains(@class, 'field--name-title')]/text()"
+            ).get(),
             "address": self.get_address(response),
             "phone_number": self.get_phone_number(response),
             "location": {
-                "indoor": bool(
-                    response.xpath(pool_location_xpath.format(pool_location="indoor"))
-                ),
-                "outdoor": bool(
-                    response.xpath(pool_location_xpath.format(pool_location="outdoor"))
-                ),
+                "indoor": "indoor" in additional_details,
+                "outdoor": "outdoor" in additional_details,
             },
             "schedule": schedule_obj,
-            "schedule_pdf_urls": response.xpath(
-                "//a[contains(translate(@href, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), 'schedule') and contains(@href, '.pdf')]/@href"
-            ).getall(),
+            "schedule_pdf_urls": schedule_pdf_urls,
             "alert": self.get_alert(response),
             "lat": response.xpath("//meta[@property='latitude']/@content").get(),
             "lon": response.xpath("//meta[@property='longitude']/@content").get(),
@@ -85,8 +87,9 @@ class ChiParksPoolSpider(scrapy.Spider):
         }
 
     def parse(self, response):
+
         for pool_detail in response.xpath(
-            "//article[@itemprop='location']//h3[@class='thumbnail-object-title']/a/@href"
+            "//h3[contains(@class, 'facility--title')]/a/@href"
         ).getall():
             yield scrapy.Request(
                 response.urljoin(pool_detail),
